da0c9060a7d6e582ba8d9e906d23a4289d537682
Cplhardcore <135324281+Cplhardcore@users.noreply.github.com>
2023-09-14 07:44:48 -0700

    Add files via upload

---
+++ meta.cpp
@@ -0,0 +1,4 @@
+protocol = 1;
+publishedid = 2972483700;
+name = "Greenmag Logistics Addon";
+timestamp = 5249876292397163733;
---
+++ mod.cpp
@@ -0,0 +1,19 @@
+name = "GreenMag Logistics Addon";
+author = "4 Rifles Milsim";
+picture 	 = ""; 	// Picture displayed from the expansions menu. Optimal size is 2048x1024
+logoSmall	 = "";	// Display next to the item added by the mod
+logo		 = "";			// Logo displayed in the main menu
+//logoOver	 = "";		// When the mouse is over, in the main menu
+//action		 = "https://example.org/my-mod-page";			// Website URL, that can accessed from the expansions menu
+//tooltipOwned = "";
+
+dlcColor[]=
+{
+    0.23,
+    0.39,
+    0.3,
+    1
+};
+
+overview = "";
+hideName = 0;
